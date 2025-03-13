812788ea30bae4df49e67243f69332ff61b3efb1
Nicolas Frias <pnicolasfrias@gmail.com>
2025-03-13 15:02:01 -0300

    actualizaciones

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import {Menu} from "semantic-ui-react";
+import { useNavigate, useLocation } from "react-router";
+import './Header.scss';
+
+export default function Header() {
+
+    const currentPath = useLocation();
+    const finalCurrentPath = currentPath.pathname.replace("/", "");
+
+    const [activeItem, setActiveItem] = useState(finalCurrentPath);
+    const navigate = useNavigate()
+
+    const handleItemClick = (e, {name}) => {
+        setActiveItem(name)
+        navigate(`/${name}`)
+    }
+
+    return (
+        <div className='header-menu'>
+            <Menu secondary>
+                <Menu.Item name="inicio" active={activeItem === "inicio"} onClick={handleItemClick} />
+                <Menu.Item name="series" active={activeItem === "series"} onClick={handleItemClick} />
+                <Menu.Item name="comics" active={activeItem === "comics"} onClick={handleItemClick} />
+            </Menu>
+        </div>
+    )
+}
